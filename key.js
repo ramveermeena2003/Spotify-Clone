9bb042fc48e24f85785009ce9e257997daa6c687
ramveermeena2003 <ramveermeena2003@gmail.com>
2024-12-14 22:28:51 +0530

    first commit

--- key.js
+++ key.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 // add eventlistener to  space keyward to pausa and play the music
 document.addEventListener("keydown", function (event) {
@@ -26,4 +27,34 @@ document.addEventListener('keydown', function (event) {
             document.querySelector(".volume").getElementsByTagName("img")[0].src = "svg/mute.svg";   
         }
     }
+=======
+
+// add eventlistener to  space keyward to pausa and play the music
+document.addEventListener("keydown", function (event) {
+    if (event.code === "Space") {
+        if (!currentSong.paused) {
+            // If the audio is playing, pause it
+            currentSong.pause();
+            play.src = "svg/currentsong.svg";
+        }
+        else {
+            currentSong.play();
+            play.src = "svg/pause.svg";
+        }
+    }
+});
+
+// add event listener to keyword 'M' to mute and unmute the music
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'm' || event.key === 'M'){
+        if(currentSong.muted){
+            currentSong.muted = !currentSong.muted; // Toggles the mute status
+            document.querySelector(".volume").getElementsByTagName("img")[0].src = "svg/volume.svg";
+        }
+        else{
+            currentSong.muted = !currentSong.muted; // Toggles the mute status
+            document.querySelector(".volume").getElementsByTagName("img")[0].src = "svg/mute.svg";   
+        }
+    }
+>>>>>>> 343d373 (first commit)
 });
